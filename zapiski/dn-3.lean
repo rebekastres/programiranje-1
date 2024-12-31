8960b3b8fef389d934fb9f1daac0de032725cbea
Rebeka Stres <153rstres@gmail.com>
2024-12-31 01:09:04 +0100

    resena domaca naloga 3 - še nekaj napak

--- zapiski/dn-3.lean
+++ zapiski/dn-3.lean
@@ -10,13 +10,44 @@ set_option autoImplicit false
 ------------------------------------------------------------------------------/
 
 def vsota_prvih : Nat → Nat :=
+  fun n =>
+    let rec sestej (m : Nat) (vs : Nat) : Nat :=
+      match m with
+      | 0 => vs
+      | m' + 1 => sestej m' (vs + m)
+    sestej n 0
+
+theorem vsota_vsote (m n : Nat) : vsota_prvih (m + n) = m * n + vsota_prvih m + vsota_prvih n :=
   sorry
 
-theorem gauss : (n : Nat) → 2 * vsota_prvih n = n * (n + 1) := by
-  sorry
+theorem gauss : (n : Nat) → 2 * vsota_prvih n = n * (n + 1) :=
+  by
+    intro n
+    induction n with
+    | zero =>
+      simp [vsota_prvih]
+      rfl
+    | succ n' ih =>
+      rw [Nat.add_mul, Nat.mul_add, Nat.mul_one, Nat.one_mul]
+      rw (config := { occs := .pos [2]}) [Nat.add_assoc]
+      rw [← Nat.two_mul, Nat.mul_one]
+      rw [vsota_vsote, Nat.mul_one]
+      rw [Nat.mul_add, Nat.mul_add]
+      rw [ih]
+      simp [vsota_prvih, vsota_prvih.sestej]
+      rw [Nat.mul_add,Nat.mul_one, ← Nat.pow_two]
+      rw [← Nat.add_assoc]
+      rw (config := { occs := .pos [4]}) [Nat.add_assoc]
+      rw [← Nat.two_mul]
+      rw (config := { occs := .pos [3]}) [Nat.add_comm]
+      rw [Nat.add_assoc]
 
-theorem cisto_pravi_gauss : (n : Nat) → vsota_prvih n = (n * (n + 1)) / 2 := by
-  sorry
+
+theorem cisto_pravi_gauss : (n : Nat) → vsota_prvih n = (n * (n + 1)) / 2 :=
+  by
+    intro n
+    rw [← gauss n] -- Use the proven gauss theorem
+    simp [Nat.mul_div_cancel] -- Simplify the result using division by 2
 
 /------------------------------------------------------------------------------
  ## Vektorji
@@ -40,13 +71,23 @@ def stakni : {A : Type} → {m n : Nat} → Vektor A m → Vektor A n → Vektor
   | .sestavljen x xs' => by rw [Nat.add_right_comm]; exact Vektor.sestavljen x (stakni xs' ys)
 
 def obrni : {A : Type} → {n : Nat} → Vektor A n → Vektor A n :=
-  sorry
+  fun {A} {n} xs =>
+    let rec pomozna {m : Nat} (xs : Vektor A m) (acc : Vektor A n) :=
+      match xs with
+      | .prazen => acc
+      | .sestavljen x xs' => pomozna xs' (Vektor.sestavljen x acc)
+    pomozna xs Vektor.prazen
 
-def glava : sorry :=
-  sorry
 
-def rep : sorry :=
-  sorry
+def glava : {A : Type} → {n : Nat} → Vektor A (n + 1) -> A :=
+  fun {A} {n} xs =>
+    match xs with
+    | .sestavljen x xs' => x
+
+def rep : {A : Type} → {n : Nat} → Vektor A (n + 1) → Vektor A n :=
+  fun {A} {n} xs =>
+    match xs with
+    | .sestavljen _ xs' => xs'
 
 /------------------------------------------------------------------------------
  ## Predikatni račun
@@ -59,17 +100,42 @@ def rep : sorry :=
 
 theorem forall_implies : {A : Type} → {P Q : A → Prop} →
   (∀ x, (P x → Q x)) → (∀ x, P x) → (∀ x, Q x) := by
-  sorry
+  intros A P Q h1 h2 x
+  apply h1
+  apply h2
 
 theorem forall_implies' : {A : Type} → {P : Prop} → {Q : A → Prop} →
   (∀ x, (P → Q x)) ↔ (P → ∀ x, Q x) := by
-  sorry
+    intros A P Q
+    apply Iff.intro
+    -- Dokaz leve strani
+    intros h p x
+    exact h x p
+    -- Dokaz desne strani
+    intros h1 x h2
+    apply h1
+    apply h2
 
 theorem paradoks_pivca :
   {G : Type} → {P : G → Prop} →
   (g : G) →  -- (g : G) pove, da je v gostilni vsaj en gost
-  ∃ (p : G), (P p → ∀ (x : G), P x) := by
-  sorry
+  ∃ (p : G), (P p → ∀ (x : G), P x) :=
+  by
+    intros G P g
+    by_cases hp : ∃ x, P x
+    case pos =>
+      cases hp with
+      | intro p h =>
+        exists p
+        intro h'
+        intro x
+        exact h'
+    case neg =>
+      exists g
+      intro h'
+      exfalso
+      exact hp ⟨g, h'⟩
+
 
 /------------------------------------------------------------------------------
  ## Dvojiška drevesa
@@ -110,14 +176,41 @@ def elementi' : {A : Type} → Drevo A → List A :=
 theorem zrcali_zrcali :
   {A : Type} → (t : Drevo A) →
   zrcali (zrcali t) = t := by
+  intro A
+  intro t
+  induction t with
+  | prazno =>
+    simp [zrcali]
+  | sestavljeno x l r ihl ihr =>
+    simp [zrcali]
+    rw [ihl, ihr]
+    simp [zrcali]
+
+theorem max_comm {a b : Nat} : max a b = max b a :=
   sorry
 
 theorem visina_zrcali :
   {A : Type} → (t : Drevo A) →
   visina (zrcali t) = visina t := by
-  sorry
+  intro A
+  intro t
+  induction t with
+  | prazno =>
+    simp [zrcali, visina]
+  | sestavljeno l x r ihl ihr =>
+    simp [zrcali, visina]
+    rw [ihl, ihr]
+    rw [max_comm]
 
 theorem elementi_elementi' :
   {A : Type} → (t : Drevo A) →
   elementi t = elementi' t := by
-  sorry
+  intros A t
+  induction t with
+  | prazno =>
+    simp [elementi, elementi']
+    rfl
+  | sestavljeno l x r ihl ihr =>
+    simp [elementi, elementi']
+    rw [ihl, ihr]
+    rw [elementi'.aux]
